7c133b80081cecd9d581a411d4b24a7516778ddc
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-01-26 00:12:49 +0200

    add new attributes in meal model class to display ingredients

--- app/src/main/java/com/example/mealsapp/data/network/MealApiService.java
+++ app/src/main/java/com/example/mealsapp/data/network/MealApiService.java
@@ -1,4 +1,33 @@
 package com.example.mealsapp.data.network;
 
-public class MealApiService {
+import com.example.mealsapp.data.model.CategoriesResponse;
+import com.example.mealsapp.data.model.MealsResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface MealApiService {
+
+    @GET("categories.php")
+    Call<CategoriesResponse> getCategories();
+
+    @GET("random.php")
+    Call<MealsResponse> getRandomMeal();
+
+    @GET("filter.php")
+    Call<MealsResponse> getMealsByCategory(@Query("c") String category);
+    @GET("lookup.php")
+    Call<MealsResponse> getMealDetails(@Query("i") String id);
+    @GET("search.php")
+    Call<MealsResponse> searchByName(@Query("s") String name);
+
+    @GET("filter.php")
+    Call<MealsResponse> filterByIngredient(@Query("i") String ingredient);
+
+    @GET("filter.php")
+    Call<MealsResponse> filterByArea(@Query("a") String area);
+
+
 }
+
---
+++ app/src/main/java/com/example/mealsapp/ui/main/adapters/IngredientsAdapter.java
@@ -0,0 +1,53 @@
+package com.example.mealsapp.ui.main.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mealsapp.R;
+
+import org.jspecify.annotations.NonNull;
+
+import java.util.List;
+
+public class IngredientsAdapter
+        extends RecyclerView.Adapter<IngredientsAdapter.ViewHolder> {
+
+    private final List<String> list;
+
+    public IngredientsAdapter(List<String> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(
+            @NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_ingredient, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvIngredient.setText(list.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvIngredient;
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvIngredient = itemView.findViewById(R.id.tvIngredient);
+        }
+    }
+}
+
