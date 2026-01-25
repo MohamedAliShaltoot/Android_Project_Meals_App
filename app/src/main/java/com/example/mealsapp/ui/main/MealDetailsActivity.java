dc164c9e4261293e15d52918b65c5d3fc114ecdc
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-01-26 00:17:03 +0200

    Modify details screen to display ingredients and video . And will show also images for ingredients

--- app/src/main/java/com/example/mealsapp/ui/main/MealDetailsActivity.java
+++ app/src/main/java/com/example/mealsapp/ui/main/MealDetailsActivity.java
@@ -1,17 +1,30 @@
-package com.example.mealsapp.ui.details;
-
+package com.example.mealsapp.ui.main;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.mealsapp.R;
+import com.example.mealsapp.data.database.localDatabase.FavoriteMeal;
+import com.example.mealsapp.data.database.localDatabase.MealsDatabase;
 import com.example.mealsapp.data.model.Meal;
 import com.example.mealsapp.data.model.MealsResponse;
 import com.example.mealsapp.data.network.RetrofitClient;
+import com.example.mealsapp.ui.main.adapters.IngredientsAdapter;
+import com.example.mealsapp.utils.AppSnackbar;
+import com.example.mealsapp.utils.SnackType;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+
+import org.jspecify.annotations.NonNull;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,7 +33,14 @@ import retrofit2.Response;
 public class MealDetailsActivity extends AppCompatActivity {
 
     ImageView imgMeal;
+    ImageButton btnFavorite;
     TextView tvName, tvCountry, tvSteps;
+    RecyclerView rvIngredients;
+    YouTubePlayerView youTubePlayerView;
+
+    MealsDatabase db;
+    Meal currentMeal;
+    boolean isFavorite = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +50,26 @@ public class MealDetailsActivity extends AppCompatActivity {
         String mealId = getIntent().getStringExtra("meal_id");
 
         imgMeal = findViewById(R.id.imgMeal);
+        btnFavorite = findViewById(R.id.btnFavorite);
         tvName = findViewById(R.id.tvMealName);
         tvCountry = findViewById(R.id.tvCountry);
         tvSteps = findViewById(R.id.tvSteps);
+        youTubePlayerView = findViewById(R.id.youtubePlayerView);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        rvIngredients = findViewById(R.id.rvIngredients);
+        rvIngredients.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        );
+
+        db = MealsDatabase.getInstance(this);
 
         loadMealDetails(mealId);
+
+        btnFavorite.setOnClickListener(v -> toggleFavorite());
     }
 
     private void loadMealDetails(String id) {
-
         RetrofitClient.getApi()
                 .getMealDetails(id)
                 .enqueue(new Callback<MealsResponse>() {
@@ -47,30 +78,93 @@ public class MealDetailsActivity extends AppCompatActivity {
                                            Response<MealsResponse> response) {
 
                         if (response.isSuccessful()
-                                && response.body() != null) {
+                                && response.body() != null
+                                && response.body().getMeals() != null) {
 
-                            Meal meal = response.body().getMeals().get(0);
+                            currentMeal = response.body().getMeals().get(0);
 
-                            tvName.setText(meal.getStrMeal());
+                            tvName.setText(currentMeal.getStrMeal());
                             tvCountry.setText(
-                                    meal.getStrArea() == null ? "Unknown" : meal.getStrArea()
+                                    currentMeal.getStrArea() == null ? "Unknown" : currentMeal.getStrArea()
                             );
-
                             tvSteps.setText(
-                                    meal.getStrInstructions() == null ? "No instructions available" : meal.getStrInstructions()
+                                    currentMeal.getStrInstructions() == null
+                                            ? "No instructions available"
+                                            : currentMeal.getStrInstructions()
                             );
-
 
                             Glide.with(MealDetailsActivity.this)
-                                    .load(meal.getStrMealThumb())
+                                    .load(currentMeal.getStrMealThumb())
                                     .into(imgMeal);
+
+                            isFavorite = db.favoriteMealDao()
+                                    .isFavorite(currentMeal.getIdMeal());
+                            IngredientsAdapter adapter =
+                                    new IngredientsAdapter(currentMeal.getIngredientsList());
+
+                            rvIngredients.setAdapter(adapter);
+                            String youtubeUrl = currentMeal.getStrYoutube();
+
+                            if (youtubeUrl != null && !youtubeUrl.isEmpty()) {
+                                String videoId = youtubeUrl.substring(youtubeUrl.lastIndexOf("=") + 1);
+
+                                youTubePlayerView.addYouTubePlayerListener(
+                                        new AbstractYouTubePlayerListener() {
+                                            @Override
+                                            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                                                youTubePlayer.cueVideo(videoId, 0);
+                                            }
+                                        }
+                                );
+                            }
+
+                            updateFavoriteIcon();
                         }
                     }
 
                     @Override
-                    public void onFailure(Call<MealsResponse> call, Throwable t) {
-                    }
+                    public void onFailure(Call<MealsResponse> call, Throwable t) {}
                 });
     }
-}
 
+    private void toggleFavorite() {
+        if (currentMeal == null) return;
+
+        FavoriteMeal fav = new FavoriteMeal(
+                currentMeal.getIdMeal(),
+                currentMeal.getStrMeal(),
+                currentMeal.getStrMealThumb()
+        );
+
+        if (isFavorite) {
+            db.favoriteMealDao().delete(fav);
+            AppSnackbar.show(
+                    btnFavorite,
+                    currentMeal.getStrMeal() + " Removed from favorites",
+                    SnackType.INFO
+            );
+
+        } else {
+            db.favoriteMealDao().insert(fav);
+            AppSnackbar.show(
+                    btnFavorite,
+                    currentMeal.getStrMeal() + " Added to favorites",
+                    SnackType.SUCCESS
+            );
+
+
+        }
+
+        isFavorite = !isFavorite;
+        updateFavoriteIcon();
+    }
+
+
+    private void updateFavoriteIcon() {
+        btnFavorite.setImageResource(
+                isFavorite ? R.drawable.ic_heart_filled : R.drawable.ic_heart_outline
+        );
+
+
+    }
+}
